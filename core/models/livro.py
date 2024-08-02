1c41e0f89e9d459968ad74056416fbb3c8faf06a
giulianobre <giuliarnobre@gmail.com>
2024-08-02 08:49:18 -0300

    Incluindo relacionamento n para n no modelo do Livro

--- core/models/livro.py
+++ core/models/livro.py
@@ -9,8 +9,7 @@ class Livro(models.Model):
     isbn = models.CharField(max_length=32, null=True, blank=True)
     quantidade = models.IntegerField(default=0,  null=True, blank=True)
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
-    autor = models.ForeignKey(Autor, on_delete=models.PROTECT, related_name="livros", null=True, blank=True)
-    coautor = models.ForeignKey(Autor, on_delete=models.PROTECT, related_name="livrosCoautor", null=True, blank=True)
+    autor = models.ManyToManyField(Autor, related_name="livros")
     editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros", null=True, blank=True)
     categoria = models.ForeignKey(
         Categoria, on_delete=models.PROTECT, related_name="livros", null=True, blank=True
