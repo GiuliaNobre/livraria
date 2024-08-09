1d3bb91ea32bfe67c2751ce021f068abba79ce72
giulianobre <giuliarnobre@gmail.com>
2024-08-09 08:54:12 -0300

    Múltiplos serializadores para Livro e reformatação dos arquivos com isort e black

--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -8,8 +8,14 @@ class LivroSerializer(ModelSerializer):
         model = Livro
         fields = "__all__"
 
+
 class LivroDetailSerializer(ModelSerializer):
     class Meta:
         model = Livro
         fields = "__all__"
         depth = 1  
+
+class LivroListSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = ["id", "titulo", "preco"]
