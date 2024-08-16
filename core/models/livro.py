60838deaf04830069e6e2ef6654ccf5e53607c68
giulianobre <giuliarnobre@gmail.com>
2024-08-16 09:28:36 -0300

    Load de dados

--- core/models/livro.py
+++ core/models/livro.py
@@ -13,7 +13,7 @@ class Livro(models.Model):
     isbn = models.CharField(max_length=32, null=True, blank=True)
     quantidade = models.IntegerField(default=0,  null=True, blank=True)
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
-    autor = models.ManyToManyField(Autor, related_name="livros")
+    autores = models.ManyToManyField(Autor, related_name="livros")
     editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros", null=True, blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="livros", null=True, blank=True)
 
